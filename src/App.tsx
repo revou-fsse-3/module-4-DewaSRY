09b252981676819385484e06f57e1c08dd7de2a0
DewaSRY <sdewa6645@gmail.com>
2023-12-18 06:13:23 +0800

    inititate react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, Space, DatePicker, version } from "antd";
+
+const App = () => (
+  <div style={{ padding: "0 24px" }}>
+    <h1>antd version: {version}</h1>
+    <Space>
+      <DatePicker />
+      <Button type="primary">Primary Button</Button>
+    </Space>
+  </div>
+);
+
+export default App;
